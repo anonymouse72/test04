de5983d89726ba0eb9c0463ce5dd348336df8254
qinjing <qinjing_org@163.com>
2017-08-09 21:21:12 +0800

    vue项目第一次提交

---
+++ src/components/search/search.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="tmpl">
+		search.vue
+	</div>
+</template>
+
+<script>
+
+	export default{
+		data(){
+			return {
+
+			}
+		}
+	}
+
+</script>
+
+<style></style>
---
+++ src/components/shopcar/shopcar.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="tmpl">
+		shop.vue
+	</div>
+</template>
+
+<script>
+
+	export default{
+		data(){
+			return {
+
+			}
+		}
+	}
+
+</script>
+
+<style></style>
